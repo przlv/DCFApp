e922d47c0e74d25ab909ee4bea4a093f08abba36
Vladislav <vladprzlv@yandex.ru>
2023-05-24 12:39:00 +0500

    init

---
+++ app.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+
+class MyApp(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Мое приложение")
+        self.geometry("1280x720")
+        self.resizable(False, False)
+        # Добавьте здесь свои виджеты и компоненты интерфейса
+        
+        self.mainloop()
+
+if __name__ == "__main__":
+    app = MyApp()
